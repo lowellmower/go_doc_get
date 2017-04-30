777aa6ee804588faeeca0f7f35a61829e03fb907
Lowell Mower <lowell.m.mower@gmail.com>
2017-04-30 09:02:12 -0400

    add awareness for public and corporate repos

--- go_doc_get.py
+++ go_doc_get.py
@@ -2,6 +2,10 @@ import sublime
 import sublime_plugin
 import webbrowser
 
+def cleanPackage(pkgURI):
+	pkg = pkgURI.split('.com/')[1]
+	return pkg
+
 class GoDocGetCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
@@ -9,4 +13,14 @@ class GoDocGetCommand(sublime_plugin.TextCommand):
 		for region in view.sel():
 			selected = view.substr(region)
 
-		webbrowser.open("https://golang.org/pkg/" + selected)
+		if "github.corp" in selected:
+			# if corporate go to page
+			pkg = cleanPackage(selected)
+			webbrowser.open('https://github.corp.dyndns.com/' + pkg)
+		elif "github" in selected:
+			# if public package go to doc
+			pkg = cleanPackage(selected)
+			webbrowser.open('https://godoc.org/github.com/' + pkg)
+		else:
+			# default to golang proper
+			webbrowser.open('https://golang.org/pkg/' + selected)
